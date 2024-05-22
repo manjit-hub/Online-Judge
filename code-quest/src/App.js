70bda59f56213a8585b1028907c3b511fa21c46f
Manjit Majhi <manjitmajhi156@gmail.com>
2024-05-22 21:50:23 +0530

    SignupPageAdded

--- code-quest/src/App.js
+++ code-quest/src/App.js
@@ -1,11 +1,13 @@
 import HomePage from './pages/HomePage';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <LoginPage/>
+      {/* <HomePage /> */}
+      {/* <LoginPage/> */}
+      <SignupPage />
     </div>
   );
 }
--- code-quest/src/pages/SignupPage.js
+++ code-quest/src/pages/SignupPage.js
@@ -1,31 +1,35 @@
 import React from "react";
 import './LoginPage.css';
 import googleLogo from "./GoogleLogo.png";
-function LoginPage() {
+function SignupPage() {
     return(
         <div className="LoginPage">
             <div className="header">
                 <img src="/Assets/logo.png" alt="Logo" />
             </div>
             <div className="content">
-                <h2>Log in to CodeQuest</h2>
+                <h2>Register</h2>
                 <div className="boxes">
-                    <h5 clssName ="top">Username</h5>
+                    <h5>Full Name</h5>
+                    <input type="text" />
+                    <h5>Email</h5>
+                    <input type="text" />
+                    <h5>Username</h5>
                     <input type="text" />
                     <h5>Password</h5>
                     <input type="text" />
-                    <button className="loginPg">Log In</button>
+                    <button className="loginPg">Sign Up</button>
                     <p className="checking">or</p>
                     <button className="google">
                         <img src={googleLogo} alt="Google Logo" className="google-logo"/>Continue with Google 
                     </button>
                 </div>
                 
-                <p className="last">Don't have an account?</p>
-                <a href="" className="last">Sign up</a>
+                <p className="last">Already have an account?</p>
+                <a href="" className="last">Log In</a>
             </div>
         </div>
     );
 }
 
-export default LoginPage;
+export default SignupPage;
