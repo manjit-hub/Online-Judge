7e4a03fc377ebdb3e86bd0a65f996c49eb621895
Manjit Majhi <manjitmajhi156@gmail.com>
2024-05-25 13:52:25 +0530

    LogInAuthDone

--- code-quest/BackEnd/Authenticator/backend.js
+++ code-quest/BackEnd/Authenticator/backend.js
@@ -8,8 +8,12 @@ const app = express();
 const { DBConnection } = require("./Database/db");
 DBConnection();
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => { // Always make "/" route
     res.send("Hello World, coming from backend.js");
 });
@@ -19,7 +23,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // REGISTRATION PART
-app.post("/register", async (req, res) => { 
+app.post("/register", async (req, res) => {
     try {
         // res.send("<h1>This is register page, coming from backend.js</h1>");
         // GET ALL THE DATA FROM FRONTEND
@@ -42,22 +46,75 @@ app.post("/register", async (req, res) => {
             password: hashedPassword,
         })
         // GENERATE A TOKEN FOR USER AND SEND IT TO THE BACKEND
-        const token = jwt.sign({id:user._id, email}, process.env.SECRET_KEY, { //user._id :: Standard naming
+        const token = jwt.sign({ id: user._id, email }, process.env.SECRET_KEY, { //user._id :: Standard naming
             expiresIn: "1h",
         });
         user_token = token; // Everything will be stored in token
         user.password = undefined; // Make password to undefined -> Security purpose
 
-        res.status(200).json({message: 'You have successfuly registered !', user});
-        
-    } 
+        res.status(200).json({ message: 'You have successfuly registered !', user });
+
+    }
     catch (error) {
         console.log(error);
     }
-    
+
 });
+
+// LOGIN PART
+// LOGIN PART
+app.post("/login", async (req, res) => {
+    try {
+        // GET ALL THE DATA FROM FRONTEND
+        const { email, password } = req.body;
+
+        // CHECK ALL THE DATA IS FILLED/EXIST
+        if (!(email && password)) {
+            return res.status(400).send("Please fill all the fields");
+        }
+
+        // CHECK IF USER ALREADY EXISTS
+        const userExist = await User.findOne({ email });
+        if (!userExist) {
+            return res.status(400).send("User not found!");
+        }
+
+        // HASHING/ENCRYPT THE PASSWORD => Using bcrypt to encrypt password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // CHECK IF PASSWORD IS CORRECT
+        const isPasswordCorrect = await bcrypt.compare(password, userExist.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).send("Password is incorrect!");
+        }
+
+        // GENERATE A TOKEN FOR USER AND SEND IT TO THE BACKEND
+        const token = jwt.sign({ id: userExist._id }, process.env.SECRET_KEY, {
+            expiresIn: "1h",
+        });
+        user_token = token; // Everything will be stored in token
+        userExist.password = undefined; // Make the password undefined, so that it kept unknown => Security Purpose
+
+        // STORE TOKEN IN COOKIES
+        const options = {
+            expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+        };
+
+        // SENDING THE TOKEN
+        res.status(200).cookie("token", token, options).json({
+            message: 'You have successfully logged in!',
+            success: true,
+            token,
+        });
+    } catch (error) {
+        console.log(error); // Log the error for debugging
+        res.status(500).send("Internal server error"); // Return a generic error message
+    }
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
-    
+
 });
