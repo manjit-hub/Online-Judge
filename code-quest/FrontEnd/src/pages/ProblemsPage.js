ad09ed4287fe6f65e61d38dfaefbc5fb146efd97
Manjit Majhi <manjitmajhi156@gmail.com>
2024-06-20 20:14:31 +0530

    updated Admin check

--- code-quest/FrontEnd/src/pages/ProblemsPage.js
+++ code-quest/FrontEnd/src/pages/ProblemsPage.js
@@ -110,7 +110,7 @@ const ProblemsPage = () => {
         </button>
         
         {/* Conditional rendering for the Add Problem button */}
-        {user && user.user.Admin && (
+        {user?.user?.Admin === "true" && (
           <button className='btnPrb' onClick={() => navigate('/problems/add-problem')}>
             <img src="/Assets/addProblem.png" alt="Logo" />
             <span>{!isMinimized && 'Add Problem'}</span>
